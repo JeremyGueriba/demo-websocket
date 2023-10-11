a1a021485019799723c8e634286757128b67df06
Jérémy GUERIBA <jeremy.gueriba@gmail.com>
2023-10-11 18:55:57 +0200

    Clean WebsocketServerCommand use statement

--- src/Command/WebsocketServerCommand.php
+++ src/Command/WebsocketServerCommand.php
@@ -2,11 +2,7 @@
 
 namespace App\Command;
 
-use App\Websocket\MessageHandler;
 use App\Websocket\WebsocketServerFactory;
-use Ratchet\Http\HttpServer;
-use Ratchet\Server\IoServer;
-use Ratchet\WebSocket\WsServer;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
