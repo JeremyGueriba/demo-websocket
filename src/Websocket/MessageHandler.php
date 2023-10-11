48f89e39da0c37e935f4282ffd798d7f374b0329
Jérémy GUERIBA <jeremy.gueriba@gmail.com>
2023-10-11 18:54:08 +0200

    Implement websocket message handler

---
+++ src/Websocket/MessageHandler.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Websocket;
+
+use Ratchet\ConnectionInterface;
+use Ratchet\MessageComponentInterface;
+
+/**
+ * Class MessageHandler
+ *
+ * @package App\Websocket
+ * @author Jérémy GUERIBA
+ */
+class MessageHandler implements MessageComponentInterface
+{
+    public function __construct(private \SplObjectStorage $connections)
+    {
+    }
+
+    public function onOpen(ConnectionInterface $connection): void
+    {
+        $this->connections->attach($connection);
+    }
+
+    public function onMessage(ConnectionInterface $from, $message): void
+    {
+        foreach ($this->connections as $connection) {
+            if (!$this->connections->contains($from)) {
+                $connection->send($message);
+            }
+        }
+    }
+
+    public function onClose(ConnectionInterface $connection): void
+    {
+        $this->connections->detach($connection);
+    }
+
+    public function onError(ConnectionInterface $connection, \Exception $e): void
+    {
+        $this->connections->detach($connection);
+        $connection->close();
+    }
+}
